cca6ebacbec2dc1ca8088039c8d55f84f8b687bf
HojuShin <shin03188@naver.com>
2023-02-26 07:03:34 +0900

    feat : header bar

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+import logoImg from '../assets/images/logo.png';
+
+const Logo = styled.div`
+height : 50px;
+width: 143px;
+margin : 0 auto ;
+padding : 5px 0;
+`;
+
+function Header() {
+    return (
+        <Logo>
+            <img src={logoImg} style={{ 'width': '143px', 'height': '50px' }}></img>
+        </Logo>
+    )
+}
+export default Header; 
