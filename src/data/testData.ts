97c4fe657453dbe2b40438439d657cc8a298c42e
ryan-dia <cjfdnjs158@naver.com>
2023-02-26 04:49:14 +0900

    feat: data 추가 및 수정

--- src/data/testData.ts
+++ src/data/testData.ts
@@ -4,14 +4,14 @@ const data: Data = {
   result: [
     {
       poster_path:
-        'https://user-images.githubusercontent.com/76567238/221348823-206633b2-52dd-40a0-b808-c42b2f85c4ad.png',
+        'https://user-images.githubusercontent.com/76567238/221376420-ee96140e-170c-4357-996b-9c7fcf9252e0.png',
 
-      text: '저 이 신발 300만원 인데 사도 될까요? \n 연봉 10억이에요',
+      text: 'A? B?',
       posting_date: '2022-02-25',
       id: 54,
       vote: {
         vote_count: 421,
-        vote_text: ['사자', '사지말자'],
+        vote_text: ['A', 'B'],
         vote_case_left: 21,
         vote_case_right: 400,
       },
@@ -63,6 +63,51 @@ const data: Data = {
         vote_case_right: 1300,
       },
       like: 52,
+    },
+    {
+      poster_path:
+        'https://user-images.githubusercontent.com/76567238/221370169-67d03c7e-cbef-45b7-a08c-61eae9a152d9.png',
+
+      text: '두 번째? 세 번재 ? ',
+      posting_date: '2022-03-43',
+      id: 59,
+      vote: {
+        vote_count: 5233,
+        vote_text: ['두 번째', '세 번재 '],
+        vote_case_left: 5000,
+        vote_case_right: 233,
+      },
+      like: 52,
+    },
+    {
+      poster_path:
+        'https://user-images.githubusercontent.com/76567238/221371397-192d1ddd-1d61-4778-a282-c8e0f752810a.png',
+
+      text: '이거 중고나라에 3400에 올라왔는데 사? 말어?',
+      posting_date: '2022-03-43',
+      id: 60,
+      vote: {
+        vote_count: 7233,
+        vote_text: ['사', '사지마'],
+        vote_case_left: 7000,
+        vote_case_right: 233,
+      },
+      like: 252,
+    },
+    {
+      poster_path:
+        'https://user-images.githubusercontent.com/76567238/221376368-166214d4-a37b-4eae-8dfc-91c8312e975b.png',
+
+      text: 'A? B?',
+      posting_date: '2022-01-43',
+      id: 61,
+      vote: {
+        vote_count: 11300,
+        vote_text: ['A', 'B'],
+        vote_case_left: 5640,
+        vote_case_right: 5700,
+      },
+      like: 3252,
     },
   ],
 };
