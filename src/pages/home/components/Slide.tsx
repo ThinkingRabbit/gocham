66afa1fe495bd125debe3c5fdf395601b9d3bb92
ryan-dia <cjfdnjs158@naver.com>
2023-02-25 18:56:12 +0900

    feat : content 추가 및 버튼 추가

--- src/pages/home/components/Slide.tsx
+++ src/pages/home/components/Slide.tsx
@@ -3,27 +3,39 @@ import Content from './Content';
 
 function Slide() {
   const Wrapper = styled.div`
-    background-color: whitesmoke;
+    background-color: white;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+    align-items: center;
     height: 90vh;
     min-height: 500px;
-    width: 100%;
+    padding-top: 20px;
+    padding-left: 20px;
+    padding-right: 20px;
   `;
 
   const VoteSection = styled.div`
     height: 10%;
     display: flex;
+    width: 100%;
+    height: 58px;
     margin: 0 1em 3em 1em;
+    background: #ededed;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
   `;
 
   const Button = styled.button`
     font-size: 33px;
     font-weight: 700;
-    width: 100%;
     border: 0;
-    background-color: #ededed;
+    width: 100%;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 29px;
     &:first-child {
       border-right: 0.5px solid black;
       border-radius: 2em 0 0 2em;
---
+++ src/pages/home/style/content.tsx
@@ -0,0 +1,69 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 516px;
+
+  background: #f5f5f5;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+`;
+
+export const Header = styled.div`
+  height: 45px;
+  background-color: #f5f5f5;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10px 0 10px;
+  border-radius: 20px;
+`;
+
+export const ImgBox = styled.div<{ bgphoto: string }>`
+  background-image: url(${(props) => props.bgphoto});
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 312px;
+`;
+
+export const FunctionalBox = styled.div`
+  height: 40px;
+  background: #f3f3f3;
+  box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.25);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 0 3px 0;
+  padding: 0 10px 0 10px;
+`;
+
+export const TextBox = styled.div`
+  background-color: #f5f5f5;
+  height: 56px;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 28px;
+  padding: 10px 10px 0 10px;
+`;
+
+export const VoteText = styled.span`
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 17px;
+  /* identical to box height */
+
+  color: #484848;
+`;
+
+export const IconBox = styled.div`
+  display: flex;
+  gap: 10px;
+`;
