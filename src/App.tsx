6765fcfeb1ca39ecf2f05fd20d8312597dbb7650
ryan-dia <cjfdnjs158@naver.com>
2023-02-26 15:37:30 +0900

    feat(main-page): Recoil 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import Home from './pages/home/Home';
 import { Global } from '@emotion/react';
 import { reset } from './reset';
 import { Route, Routes } from 'react-router-dom';
+import { RecoilRoot, atom, selector, useRecoilState, useRecoilValue } from 'recoil';
 
 function App() {
   return (
     <>
-      <Global styles={reset}></Global>
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/shorts/:id' element={<Home />} />
-      </Routes>
+      <RecoilRoot>
+        <Global styles={reset}></Global>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/shorts/:id' element={<Home />} />
+        </Routes>
+      </RecoilRoot>
     </>
   );
 }
