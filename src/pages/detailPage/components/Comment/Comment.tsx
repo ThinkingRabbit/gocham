92d393fa32194517f9087d597f81baa67be767a9
조윤식 <postop@tuneit.io>
2023-02-26 14:04:10 +0900

    feat(detail-page): UI 피드백, props 추출

--- src/pages/detailPage/components/Comment/Comment.tsx
+++ src/pages/detailPage/components/Comment/Comment.tsx
@@ -1,43 +1,45 @@
-import React, {useState} from 'react';
+import React, { useState} from 'react';
 import * as S from "./Comment.style";
 import IUser from "../../../../assets/icon/icon_user.png";
 import ILike from "../../../../assets/icon/icon_like.png";
 import ILikeF from "../../../../assets/icon/icon_like_filled.png";
+import {CommentType} from "../../type/comment.type";
 
-const Comment = () => {
-  const [isLike, setIsLike] = useState(false);
+const Comment = (props: CommentType) => {
+  const { id, comment, like, profile, selected, name, isBlue} = props;
+  const [isLiked, setIsLiked] = useState(like.isLiked);
+  const [likeCount, setLikeCount] = useState(like.likeCount);
 
-  const comment = {
-    name: "디아",
-    profile: "",
-    likeCount: 1234,
-    comment: "내용이 작성됩니다.내용이 작성됩니다.내용이 작성됩니다.내용이 작성됩니다.내용이 작성됩니다.내용이 작성됩니다.",
-    selected: "신발을 산다",
-    isBlue: true,
-    isLiked: false,
-  }
-
+  // TODO - 데이터의 isLiked 값으로 변경
   const isLikeClicked = () => {
-    setIsLike(prev => !prev);
+    setIsLiked(prev => !prev);
+    isLike();
   };
 
+  const isLike = () => {
+    if (props.like.isLiked) {
+      // TODO - 좋아요 취소 API 요청
+      return;
+    }
+    //  TODO -  좋아요 API 요청
+  }
 
   return (
     <S.CommentWrapper>
       <S.InfoWrapper>
         <S.WriterWrapper>
-          <S.ImgUser src={comment.profile ? comment.profile : IUser} alt=""/>
-          <S.Name>{comment.name}</S.Name>
-          <S.Badge isBlue={comment.isBlue}>{comment.selected}</S.Badge>
+          <S.ImgUser src={profile ? profile : IUser} alt=""/>
+          <S.Name>{name}</S.Name>
+          <S.Badge isBlue={isBlue}>{selected}</S.Badge>
         </S.WriterWrapper>
         <S.LikeWrapper>
           <S.BtnLike type="button" onClick={isLikeClicked}>
-            <img src={isLike ? ILikeF : ILike} alt="좋아요"/>
+            <img src={isLiked ? ILikeF : ILike} alt="좋아요"/>
           </S.BtnLike>
-          <S.LikeCount>{comment.likeCount.toLocaleString()}</S.LikeCount>
+          <S.LikeCount>{like.likeCount.toLocaleString()}</S.LikeCount>
         </S.LikeWrapper>
       </S.InfoWrapper>
-      <S.Comment>{comment.comment}</S.Comment>
+      <S.Comment>{comment}</S.Comment>
     </S.CommentWrapper>
   );
 };
--- src/pages/detailPage/index.tsx
+++ src/pages/detailPage/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import Comment from "./components/Comment/Comment";
+import {CommentType} from "./type/comment.type";
 
 const Index = () => {
+  const comment: CommentType = {
+    id: 1,
+    name: "디아",
+    profile: "",
+    comment: "내용이 작성됩니다.내용이 작성됩니다.내용이 작성됩니다.내용이 작성됩니다.내용이 작성됩니다.내용이 작성됩니다.",
+    selected: "신발을 산다",
+    isBlue: true,
+    like: {
+      isLiked: false,
+      likeCount: 1234,
+    }
+  }
+
   return (
     <div>
       <h2>상세조회 페이지</h2>
-      <Comment/>
+      <Comment {...comment}/>
     </div>
   );
 };
