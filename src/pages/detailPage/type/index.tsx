16eb7258c16e0b70ad32949158423c84b37cc8f7
zinukk <slsxpseh54@naver.com>
2023-02-26 18:52:07 +0900

    Fix : resolve conflict

--- src/pages/detailPage/type/index.tsx
+++ src/pages/detailPage/type/index.tsx
@@ -1,18 +1,7 @@
 import React from 'react';
 
 const Index = () => {
-<<<<<<< HEAD
   return <div>dommy</div>;
 };
 
 export default Index;
-=======
-  return (
-    <div>
-      dommy
-      </div>
-  );
-};
-
-export default Index;
->>>>>>> 92d393fa32194517f9087d597f81baa67be767a9
